e60c8cbe328fd6a4851b49c2fe30a4e9dfd4db30
souvenir718 <mongkey0@naver.com>
2021-02-16 11:20:50 +0900

    how to use css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -19,6 +21,6 @@ export default function FirstPost() {
                 width={144} // Desired size with correct aspect ratio
                 alt="Souvenir"
             />
-        </>
+        </Layout>
     );
 }
