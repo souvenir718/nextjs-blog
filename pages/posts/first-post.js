b3fc365617bae6be045d37dad69a0cffc5be571b
souvenir718 <mongkey0@naver.com>
2021-02-16 10:44:11 +0900

    Image component from 'next/image'

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function FirstPost() {
     return (
         <>
@@ -9,6 +9,12 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
+            <Image
+                src="/images/profile.jpg" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Souvenir"
+            />
         </>
     );
 }
